f772be7eb99438c77f7c84792df9588016b09193
Oscar <xworld_of_dreams@hotmail.com>
2017-04-20 14:30:32 -0300

    creacion tienda

--- app/src/main/java/com/chile/oscar/clickcomida_aplicacionmovil/fragmentTienda.java
+++ app/src/main/java/com/chile/oscar/clickcomida_aplicacionmovil/fragmentTienda.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
 
 
 /**
@@ -24,6 +25,8 @@ public class fragmentTienda extends Fragment
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
+    RadioButton radioContinuado, radioTomorrow;
+
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -33,6 +36,7 @@ public class fragmentTienda extends Fragment
     public fragmentTienda()
     {
         // Required empty public constructor
+
     }
 
     /**
@@ -44,7 +48,8 @@ public class fragmentTienda extends Fragment
      * @return A new instance of fragment fragmentTienda.
      */
     // TODO: Rename and change types and number of parameters
-    public static fragmentTienda newInstance(String param1, String param2) {
+    public static fragmentTienda newInstance(String param1, String param2)
+    {
         fragmentTienda fragment = new fragmentTienda();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
@@ -54,9 +59,11 @@ public class fragmentTienda extends Fragment
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
+        if (getArguments() != null)
+        {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
@@ -64,20 +71,24 @@ public class fragmentTienda extends Fragment
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                             Bundle savedInstanceState)
+    {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_fragment_tienda, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
+    public void onButtonPressed(Uri uri)
+    {
+        if (mListener != null)
+        {
             mListener.onFragmentInteraction(uri);
         }
     }
 
     @Override
-    public void onAttach(Context context) {
+    public void onAttach(Context context)
+    {
         super.onAttach(context);
         if (context instanceof OnFragmentInteractionListener) {
             mListener = (OnFragmentInteractionListener) context;
@@ -88,7 +99,8 @@ public class fragmentTienda extends Fragment
     }
 
     @Override
-    public void onDetach() {
+    public void onDetach()
+    {
         super.onDetach();
         mListener = null;
     }
@@ -103,8 +115,10 @@ public class fragmentTienda extends Fragment
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
      */
-    public interface OnFragmentInteractionListener {
+    public interface OnFragmentInteractionListener
+    {
         // TODO: Update argument type and name
+
         void onFragmentInteraction(Uri uri);
     }
 }
