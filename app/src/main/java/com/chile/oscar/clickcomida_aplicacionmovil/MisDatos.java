268661e53ad4bb83e3349d553d36f771572dd477
Oscar <xworld_of_dreams@hotmail.com>
2017-09-05 14:14:09 -0300

    Mantención 7 (Cambio de diseño en mis datos y codigo optimizado)

--- app/src/main/java/com/chile/oscar/clickcomida_aplicacionmovil/MisDatos.java
+++ app/src/main/java/com/chile/oscar/clickcomida_aplicacionmovil/MisDatos.java
@@ -3,6 +3,8 @@ package com.chile.oscar.clickcomida_aplicacionmovil;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
@@ -11,6 +13,7 @@ import android.os.AsyncTask;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AlertDialog;
 import android.text.Html;
 import android.view.LayoutInflater;
@@ -24,6 +27,8 @@ import android.widget.ListView;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.chile.oscar.clickcomida_aplicacionmovil.Clases.Coordenadas;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -46,6 +51,8 @@ public class MisDatos extends Fragment
     ImageView imageViewModificar;
     Button buttonAddTel, buttonAddDir;
     ListView listViewTel, listViewDir;
+    int id;
+    String tipoUpdate = "";
 
     List<String> listTelefono;
 
@@ -69,8 +76,7 @@ public class MisDatos extends Fragment
 
         listTelefono = new ArrayList<>();
 
-        Bundle bundle = getArguments();
-        int id = Integer.parseInt(bundle.getString("IdUser"));
+        cargarPreferencias();
 
         try
         {
@@ -83,9 +89,71 @@ public class MisDatos extends Fragment
             e.printStackTrace();
         }
 
+        buttonAddTel.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                AlertDialog.Builder builderM = new AlertDialog.Builder(getContext());
+                View p = getActivity().getLayoutInflater().inflate(R.layout.modificar_telefono, null);
+
+                final EditText editTextTelefono = (EditText)p.findViewById(R.id.etNuevoTelefonp_us);;
+                final String tel = editTextTelefono.getText().toString();
+                builderM.setView(p);
+                final AlertDialog dialog = builderM.create();
+
+                Button buttonMod = (Button)p.findViewById(R.id.btnModificarTelefono_us);
+                Button buttonCerrar = (Button)p.findViewById(R.id.btnCerrarTelefono_us);
+
+                buttonMod.setText("Agregar");
+
+                buttonMod.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        if (listTelefono.contains(editTextTelefono.getText().toString()))
+                        {
+                            Toast.makeText(getContext(), "El numero " + editTextTelefono.getText().toString()  + " ya lo tienes registrado.", Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            JSONObject jsonObjectSend = new JSONObject();
+                            try
+                            {
+                                tipoUpdate = "Telefono";
+                                jsonObjectSend.put("Id", id);
+                                jsonObjectSend.put("Antiguo", tel);
+                                jsonObjectSend.put("Nuevo", editTextTelefono.getText().toString());
+                                new EjecutarInstruccion().execute(getResources().getString(R.string.direccion_web) + "Controlador/actualizar_telefono_usuario.php", jsonObjectSend.toString());
+                            }
+                            catch (JSONException e)
+                            {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                });
+
+                buttonCerrar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        dialog.dismiss();
+                    }
+                });
+
+                dialog.show();
+            }
+        });
+
         return view;
     }
 
+    public void cargarPreferencias()
+    {
+        SharedPreferences sharedpreferences = getActivity().getSharedPreferences("datos_del_usuario", Context.MODE_PRIVATE);
+        id = Integer.parseInt(sharedpreferences.getString("id_usuario_shared", ""));
+    }
 
     private Location getMyLocation()
     {
@@ -230,24 +298,97 @@ public class MisDatos extends Fragment
                                 @Override
                                 public void onClick(View v)
                                 {
-                                    Toast.makeText(getContext(), "Lo has presionado.", Toast.LENGTH_SHORT).show();
                                     AlertDialog.Builder builderM = new AlertDialog.Builder(getContext());
                                     View p = getActivity().getLayoutInflater().inflate(R.layout.modificar_telefono, null);
 
-                                    EditText editTextTelefono = (EditText)p.findViewById(R.id.etNuevoTelefonp_us);
+                                    final EditText editTextTelefono = (EditText)p.findViewById(R.id.etNuevoTelefonp_us);
                                     editTextTelefono.setText(listTelefono.get(v.getId()));
-
+                                    final String tel = editTextTelefono.getText().toString();
                                     builderM.setView(p);
-                                    AlertDialog dialogChangeDireccionn = builderM.create();
-                                    dialogChangeDireccionn.show();
+                                    final AlertDialog dialog = builderM.create();
+
+                                    Button buttonMod = (Button)p.findViewById(R.id.btnModificarTelefono_us);
+                                    Button buttonCerrar = (Button)p.findViewById(R.id.btnCerrarTelefono_us);
+
+                                    buttonMod.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v)
+                                        {
+                                            if (listTelefono.contains(editTextTelefono.getText().toString()))
+                                            {
+                                                Toast.makeText(getContext(), "El numero " + editTextTelefono.getText().toString()  + " ya lo tienes registrado.", Toast.LENGTH_SHORT).show();
+                                            }
+                                            else
+                                            {
+                                                JSONObject jsonObjectSend = new JSONObject();
+                                                try
+                                                {
+                                                    tipoUpdate = "Telefono";
+                                                    jsonObjectSend.put("Id", id);
+                                                    jsonObjectSend.put("Antiguo", tel);
+                                                    jsonObjectSend.put("Nuevo", editTextTelefono.getText().toString());
+                                                    new EjecutarInstruccion().execute(getResources().getString(R.string.direccion_web) + "Controlador/actualizar_telefono_usuario.php", jsonObjectSend.toString());
+                                                }
+                                                catch (JSONException e)
+                                                {
+                                                    e.printStackTrace();
+                                                }
+                                            }
+                                        }
+                                    });
+
+                                    buttonCerrar.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v)
+                                        {
+                                            dialog.dismiss();
+                                        }
+                                    });
+
+                                    dialog.show();
+
+
                                 }
                             });
 
-                            buttonD.setOnClickListener(new View.OnClickListener() {
+                            buttonD.setOnClickListener(new View.OnClickListener()
+                            {
                                 @Override
-                                public void onClick(View v)
+                                public void onClick(final View v)
                                 {
+                                    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
+                                    builder.setTitle("¿Deseas eliminar este telefono?")
+                                            .setMessage(listTelefono.get(v.getId()))
+                                            .setPositiveButton("Eliminar",
+                                                    new DialogInterface.OnClickListener()
+                                                    {
+                                                        @Override
+                                                        public void onClick(DialogInterface dialog, int which)
+                                                        {
+                                                            JSONObject jsonObjectDelete = new JSONObject();
+                                                            try
+                                                            {
+                                                                tipoUpdate = "Eliminar telefono";
+                                                                jsonObjectDelete.put("Id", id);
+                                                                jsonObjectDelete.put("Telefono", listTelefono.get(v.getId()));
+                                                                new EjecutarInstruccion().execute(getResources().getString(R.string.direccion_web) + "Controlador/eliminar_datos_usuario.php", jsonObjectDelete.toString());
+                                                            }
+                                                            catch (JSONException e)
+                                                            {
+                                                                e.printStackTrace();
+                                                            }
+                                                        }
+                                                    })
+                                            .setNegativeButton("Cancelar",
+                                                    new DialogInterface.OnClickListener() {
+                                                        @Override
+                                                        public void onClick(DialogInterface dialog, int which)
+                                                        {
+                                                            dialog.dismiss();
+                                                        }
+                                                    });
+                                    builder.show();
                                 }
                             });
                         }
@@ -344,6 +485,101 @@ public class MisDatos extends Fragment
                     }
                 };
                 listViewDir.setAdapter(baseAdapterD);
+
+            }
+            catch (JSONException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+    public class EjecutarInstruccion extends AsyncTask<String, Void, String>
+    {
+        @Override
+        public String doInBackground(String... params)
+        {
+            HttpURLConnection conn = null;
+            try
+            {
+                StringBuffer response = null;
+                URL url = new URL(params[0]);
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(10000);
+                conn.setConnectTimeout(15000);
+                conn.setRequestProperty("Content-Type", "application/json");
+                conn.setDoOutput(true);
+                conn.setRequestMethod("POST");
+
+                OutputStream out = new BufferedOutputStream(conn.getOutputStream());
+                out.write(params[1].toString().getBytes());
+                out.close();
+
+                int responseCode = conn.getResponseCode();
+                System.out.println("responseCode" + responseCode);
+                switch (responseCode)
+                {
+                    case 200:
+                        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                        String inputLine;
+                        response = new StringBuffer();
+                        while ((inputLine = in.readLine()) != null)
+                        {
+                            response.append(inputLine);
+                        }
+                        in.close();
+                        return response.toString();
+                }
+            }
+            catch (IOException ex)
+            {
+                ex.printStackTrace();
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    try
+                    {
+                        conn.disconnect();
+                    }
+                    catch (Exception ex)
+                    {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s)
+        {
+            try
+            {
+                JSONObject jsonObject = new JSONObject(s);
+                if (tipoUpdate.equals("Telefono"))
+                {
+                    if (jsonObject.getString("Resultado").equals("Si"))
+                    {
+                        Toast.makeText(getContext(), "Numero actualizado.", Toast.LENGTH_SHORT).show();
+
+                        Fragment currentFragment = getFragmentManager().findFragmentByTag("Datos");
+                        FragmentTransaction fragTransaction = getFragmentManager().beginTransaction();
+                        fragTransaction.detach(currentFragment);
+                        fragTransaction.attach(currentFragment);
+                        fragTransaction.commit();
+                    }
+                }
+                else if (tipoUpdate.equals("Eliminar telefono"))
+                {
+                    Toast.makeText(getContext(), "Telefono eliminado.", Toast.LENGTH_SHORT).show();
+
+                    Fragment currentFragment = getFragmentManager().findFragmentByTag("Datos");
+                    FragmentTransaction fragTransaction = getFragmentManager().beginTransaction();
+                    fragTransaction.detach(currentFragment);
+                    fragTransaction.attach(currentFragment);
+                    fragTransaction.commit();
+                }
 
             }
             catch (JSONException e)
